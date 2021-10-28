18c337ddcf9b8f11fcac9af3de94c27478c79c2a
Hamza-BenJabeur <66827492+Hamza-BenJabeur@users.noreply.github.com>
2021-10-28 19:13:32 +0100

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
-const url = 'http://20.94.198.235:6868/api/pets';
+//in the FILL_ME blank put your public ip adress of the instance
+const url = 'http://"FILL_ME":6868/api/pets';
 
 
 export const Registration = (reg) => axios.post(url, reg);
